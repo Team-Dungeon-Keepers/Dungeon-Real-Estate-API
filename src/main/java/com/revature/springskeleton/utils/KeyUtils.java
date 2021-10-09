493851a706242b3c10abcbf4a9a6628feeada987
Michael-Kochis <rhishisikk@gmail.com>
2021-10-09 15:53:48 -0500

    KeyUtils.nextKey

---
+++ src/main/java/com/revature/springskeleton/utils/KeyUtils.java
@@ -0,0 +1,11 @@
+package com.revature.springskeleton.utils;
+
+import java.util.Date;
+
+public class KeyUtils {
+    public static long nextKey() {
+        Date tempDate = new Date();
+
+        return tempDate.getTime();
+    }
+}
