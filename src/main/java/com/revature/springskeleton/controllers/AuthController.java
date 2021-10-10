8ba19e03beac2af91dd860b479f1f7971bd5ad4f
Michael-Kochis <rhishisikk@gmail.com>
2021-10-09 20:43:36 -0500

    Failed to apply @ComponentScan

--- src/main/java/com/revature/springskeleton/controllers/AuthController.java
+++ src/main/java/com/revature/springskeleton/controllers/AuthController.java
@@ -6,10 +6,7 @@ import com.revature.springskeleton.repositories.UserRepository;
 import com.revature.springskeleton.utils.KeyUtils;
 import com.revature.springskeleton.utils.PasswordUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Date;
 
 @RestController
 @CrossOrigin
