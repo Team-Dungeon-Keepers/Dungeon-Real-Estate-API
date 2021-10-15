96225dae150c0b4da2866b566c1bd4bbc33dd46a
Michael-Kochis <rhishisikk@gmail.com>
2021-10-15 08:27:51 -0500

    Test of the last primary fields, link and category

---
+++ src/main/java/com/revature/dungeonsite/repositories/LinkRepository.java
@@ -0,0 +1,9 @@
+package com.revature.dungeonsite.repositories;
+
+import com.revature.dungeonsite.models.Link;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LinkRepository extends JpaRepository<Link, Long>{
+}
