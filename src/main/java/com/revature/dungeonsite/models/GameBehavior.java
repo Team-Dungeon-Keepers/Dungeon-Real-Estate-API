0dc24948cbbd568ad9bf5f6014ae997275b9e1a6
Michael-Kochis <rhishisikk@gmail.com>
2021-10-15 08:33:28 -0500

    Corrected typo in CategoryController

---
+++ src/main/java/com/revature/dungeonsite/models/GameBehavior.java
@@ -0,0 +1,26 @@
+package com.revature.dungeonsite.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="game_behavior")
+public class GameBehavior {
+    @Id
+	@Column(name="id")
+    @Getter  @Setter
+    private Long ID;
+	
+    @Column(name="gameid")
+    @Getter  @Setter
+    private Long gameID;
+	
+    @Column(name="behaviorid")
+    @Getter @Setter
+    private Long behaviorID;
+}
